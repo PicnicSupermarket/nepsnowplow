ad95bf9b370e76d62842a34879d38dd7a47daaf3
Jelmer Borst <jelmer.borst@teampicnic.com>
2018-07-25 09:09:00 +0200

    Make schema path relative

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-const networkInterfaces = require("./network_interfaces");
-const base64Decode = require("./b64.js");
+const { remote } = require("electron");
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const jsonfile = require("jsonfile");
-const { remote } = require("electron");
 
+const networkInterfaces = require("./network_interfaces");
+const base64Decode = require("./b64.js");
 const SnowplowEvent = require("./model/snowplow_event.js");
 const ValidationSchema = require("./model/validation_schema.js");
-
 const appLogger = require("./logger/app_logger.js");
 
 var app = express();
@@ -19,13 +18,22 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 var schemas = {};
 var schemaDir = remote.getGlobal("options").schemaDir;
-if (!!schemaDir && schemaDir !== "") {
-    readSchema(schemaDir);
+if (!!schemaDir) {
+    readSchema(path.join(__dirname, "..", schemaDir));
 }
 
 function readSchema(file) {
-    var stats = fs.lstatSync(file);
-    if (stats.isFile()) {
+    let stats;
+    try {
+        stats = fs.lstatSync(file);
+    } catch (err) {
+        // catch in case when file or directory could not be resolved,
+        // e.g. somebody deleted the schemas folder
+        console.log(err);
+    }
+
+    if (stats.isFile() && path.extname(file) !== ".md") {
+        // ignore readme.md
         try {
             let schemaName = file
                 .substring(schemaDir.length)
@@ -35,6 +43,7 @@ function readSchema(file) {
                 jsonfile.readFileSync(file)
             );
         } catch (err) {
+            // catch non-valid JSON schemas
             console.log(err);
         }
     } else if (stats.isDirectory(file)) {
