44e935e8e71c7da3ca44e3fa3a019e78cd5ad515
Jelmer Borst <jelmer.borst@teampicnic.com>
2018-09-13 10:47:23 +0200

    Render listening port after server start

--- app/models/Window/mainWindowRenderer.js
+++ app/models/Window/mainWindowRenderer.js
@@ -9,10 +9,10 @@ const appLogger = require("../../../server/appLogger");
 const filter = require("../../..//server/filter");
 const { Template } = require("../Template");
 const { PaneGroup, SidebarListPane, DetailsPane } = require("../Pane");
+const Server = require("../../../server/server");
 
-function startServer() {
-    require("../../../server/server");
-}
+var server = new Server();
+var serverInst = server.start();
 
 function enableSearchListener() {
     const filterEventsInput = document.getElementById("filter-events");
@@ -173,26 +173,27 @@ function renderMain(target) {
     paneGroup.show();
 }
 
-function renderFooter(target, ip) {
+function renderFooter(target, ip, port) {
     let tmpl = new Template({
         path: path.join(__dirname, "FooterToolbar.hbs"),
         parent: target
     });
-    let listeningPort = remote.getGlobal("options").listeningPort;
     let data = {
         ipAddress: ip || "...",
-        port: listeningPort
+        port: port | "..."
     };
     tmpl.render(data);
 }
 
 function updateFooter(target) {
-    network.get_active_interface((err, iface) => {
-        let footer = document.getElementById("footer");
-        footer.parentNode.removeChild(footer);
+	serverInst.on("ready", (port) => {
+		network.get_active_interface((err, iface) => {
+	        let footer = document.getElementById("footer");
+	        footer.parentNode.removeChild(footer);
 
-        renderFooter(target, iface.ip_address);
-    });
+	        renderFooter(target, iface.ip_address, port);
+	    });
+	}
 }
 
 function renderWindow() {
@@ -205,7 +206,6 @@ function renderWindow() {
 }
 
 function mainWindowRenderer() {
-    startServer();
     renderWindow();
 }
 
--- server/server.js
+++ server/server.js
@@ -13,94 +13,113 @@ const SnowplowEvent = require("./model/SnowplowEvent");
 const ValidationSchema = require("./model/ValidationSchema");
 const appLogger = require("./appLogger");
 
-var server = express();
-server.use(bodyParser.json()); // to parse application/json
-server.use(bodyParser.urlencoded({ extended: true })); // to parse application/x-www-form-urlencoded
-
-var schemas = {};
-var schemaDir = remote.getGlobal("options").schemaDir;
-
-function readSchema(file) {
-    let stats;
-    try {
-        stats = fs.lstatSync(file);
-    } catch (err) {
-        // catch in case the file or directory could not be resolved,
-        // when e.g. somebody deleted the schemas folder
-        console.log(err);
-        return;
+class Server {
+    constructor() {
+        this.schemas = {};
+        this.schemaDir = remote.getGlobal("options").schemaDir;
+        this.instance = express();
+        this.instance.use(bodyParser.json()); // to parse application/json
+        this.instance.use(bodyParser.urlencoded({ extended: true })); // to parse application/x-www-form-urlencoded
     }
 
-    if (stats.isFile() && (path.extname(file) === "" || path.extname(file) === ".json")) {
-        // only accept .json or no extension
-        try {
-            let schema = jsonfile.readFileSync(file);
-            let schemaName = [
-                schema.self.vendor,
-                schema.self.name,
-                schema.self.format,
-                schema.self.version
-            ].join("/");
-            schemas[schemaName] = new ValidationSchema(schemaName, schema);
-        } catch (err) {
-            // catch non-valid JSON schemas
-            console.log(file);
-            console.log(err);
+    start() {
+        this.readSchemas();
+        this.captureEvents();
+        this.listen();
+
+        return this.instance;
+    }
+
+    readSchemas() {
+        if (!!this.schemaDir) {
+            // in production, remove app.asar from the path
+            // cannot use process.resourcesPath in development,
+            // as that will point to electron/dist in node_modules
+            let resourcesPath = remote.app.getAppPath().replace("app.asar", "");
+            this.readSchema(path.join(resourcesPath, this.schemaDir));
         }
-    } else if (stats.isDirectory(file)) {
-        let files = fs.readdirSync(file);
-        files.forEach(function(f) {
-            readSchema(path.join(file, f));
+    }
+
+    readSchema(file) {
+        let stats;
+        try {
+            stats = fs.lstatSync(file);
+        } catch (err) {
+            // catch in case the file or directory could not be resolved,
+            // when e.g. somebody deleted the schemas folder
+            console.log(err);
+            return;
+        }
+
+        if (stats.isFile() && (path.extname(file) === "" || path.extname(file) === ".json")) {
+            // only accept .json or no extension
+            try {
+                let schema = jsonfile.readFileSync(file);
+                let schemaName = [
+                    schema.self.vendor,
+                    schema.self.name,
+                    schema.self.format,
+                    schema.self.version
+                ].join("/");
+                this.schemas[schemaName] = new ValidationSchema(schemaName, schema);
+            } catch (err) {
+                // catch non-valid JSON schemas
+                console.log(file);
+                console.log(err);
+            }
+        } else if (stats.isDirectory(file)) {
+            let files = fs.readdirSync(file);
+            files.forEach((f) => {
+                this.readSchema(path.join(file, f));
+            }, this);
+        }
+    }
+
+    listen() {
+        let port = remote.getGlobal("options").listeningPort;
+        this.instance
+            .listen(port, () => {
+                this.handleStartup(port);
+            })
+            .on("error", (err) => {
+                if (err.errno === "EADDRINUSE") {
+                    console.log("Port " + port + " in use, using random port");
+                    let randPort = Math.floor(1000 + Math.random() * 9000);
+                    this.instance.listen(randPort, () => {
+                        this.handleStartup(randPort);
+                    });
+                } else {
+                    console.log(err);
+                }
+            });
+    }
+
+    handleStartup(port) {
+        this.instance.emit("ready", port);
+        console.log("Listening for SnowPlow analytics on port " + port);
+	    console.log("Please check you both of your devices are on the same network");
+	    console.log("________________________________________________________________________________");
+	    console.log("");
+    }
+
+    captureEvents() {
+        // Capturing every post events to this server
+        this.instance.post("*", function(req, res) {
+            let body = req.body;
+
+            let bundle = body.data.reverse();
+            bundle.forEach(function(data) {
+                let context = JSON.parse(base64.decode(data.cx));
+                let payload = JSON.parse(base64.decode(data.ue_px));
+
+                let event = new SnowplowEvent(data.uid, payload, context);
+                event.validate(schemas);
+
+                appLogger.logEvent(event);
+            });
+            res.sendStatus(204);
         });
     }
 }
 
-function showStartupMessage(port) {
-    console.log("Listening for SnowPlow analytics on port " + port);
-    console.log("Please check you both of your devices are on the same network");
-    console.log("________________________________________________________________________________");
-    console.log("");
-}
-
-if (!!schemaDir) {
-    // in production, remove app.asar from the path
-    // cannot use process.resourcesPath in development,
-    // as that will point to electron/dist in node_modules
-    let resourcesPath = remote.app.getAppPath().replace("app.asar", "");
-    readSchema(path.join(resourcesPath, schemaDir));
-}
-
-// Capturing every post events to this server
-server.post("*", function(req, res) {
-    let body = req.body;
-
-    let bundle = body.data.reverse();
-    bundle.forEach(function(data) {
-        let context = JSON.parse(base64.decode(data.cx));
-        let payload = JSON.parse(base64.decode(data.ue_px));
-
-        let event = new SnowplowEvent(data.uid, payload, context);
-        event.validate(schemas);
-
-        appLogger.logEvent(event);
-    });
-    res.sendStatus(204);
-});
-
-// Start server
-let port = remote.getGlobal("options").listeningPort;
-server
-    .listen(port, function() {
-        showStartupMessage(port);
-    })
-    .on("error", (err) => {
-        if (err.errno === "EADDRINUSE") {
-            console.log("Port " + port + " in use, using random port");
-            let randPort = Math.floor(1000 + Math.random() * 9000);
-            server.listen(randPort, function() {
-                showStartupMessage(randPort);
-            });
-        } else {
-            console.log(err);
-        }
-    });
+module.exports = Server;
